df5bc5ebabf73eb3900bb7219a579b6f35ea26d3
Gaëtan Muller <m.gaetan89@gmail.com>
2015-07-05 13:20:35 +0200

    Reenable Leak Canary

--- app/src/main/java/com/mgaetan89/showsrage/ShowsRageApplication.java
+++ app/src/main/java/com/mgaetan89/showsrage/ShowsRageApplication.java
@@ -2,11 +2,13 @@ package com.mgaetan89.showsrage;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 public class ShowsRageApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
 
-		//LeakCanary.install(this);
+		LeakCanary.install(this);
 	}
 }
