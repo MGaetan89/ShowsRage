afce0bcde8933b6819df2d719b59a0c19184acf4
Gaëtan Muller <m.gaetan89@gmail.com>
2015-07-05 13:50:32 +0200

    Exit show overview after a successful deletion

--- app/src/main/java/com/mgaetan89/showsrage/fragment/ShowOverviewFragment.java
+++ app/src/main/java/com/mgaetan89/showsrage/fragment/ShowOverviewFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -559,6 +560,8 @@ public class ShowOverviewFragment extends Fragment implements Callback<SingleSho
 			@Override
 			public void success(GenericResponse genericResponse, Response response) {
 				Toast.makeText(getActivity(), genericResponse.getMessage(), Toast.LENGTH_SHORT).show();
+
+				NavUtils.navigateUpFromSameTask(getActivity());
 			}
 		};
 
