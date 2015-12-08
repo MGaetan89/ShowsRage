c7ea6c84b897f2777d64696175b19cc0d9f0ff36
Gaëtan Muller <m.gaetan89@gmail.com>
2015-12-08 19:36:50 +0100

    #47: Fix crash in "Schedule"

--- app/src/main/java/com/mgaetan89/showsrage/model/Schedule.java
+++ app/src/main/java/com/mgaetan89/showsrage/model/Schedule.java
@@ -1,8 +1,11 @@
 package com.mgaetan89.showsrage.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.SerializedName;
 
-public class Schedule {
+public class Schedule implements Parcelable {
 	@SerializedName("airdate")
 	private String airDate;
 	private String airs;
@@ -24,6 +27,31 @@ public class Schedule {
 	@SerializedName("tvdbid")
 	private int tvDbId;
 	private int weekday;
+
+	public Schedule() {
+	}
+
+	protected Schedule(Parcel in) {
+		this.airDate = in.readString();
+		this.airs = in.readString();
+		this.episode = in.readInt();
+		this.episodeName = in.readString();
+		this.episodePlot = in.readString();
+		this.indexerId = in.readInt();
+		this.network = in.readString();
+		this.paused = in.readInt();
+		this.quality = in.readString();
+		this.season = in.readInt();
+		this.showName = in.readString();
+		this.showStatus = in.readString();
+		this.tvDbId = in.readInt();
+		this.weekday = in.readInt();
+	}
+
+	@Override
+	public int describeContents() {
+		return 0;
+	}
 
 	public String getAirDate() {
 		return this.airDate;
@@ -80,4 +108,35 @@ public class Schedule {
 	public int getWeekday() {
 		return this.weekday;
 	}
+
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(this.airDate);
+		dest.writeString(this.airs);
+		dest.writeInt(this.episode);
+		dest.writeString(this.episodeName);
+		dest.writeString(this.episodePlot);
+		dest.writeInt(this.indexerId);
+		dest.writeString(this.network);
+		dest.writeInt(this.paused);
+		dest.writeString(this.quality);
+		dest.writeInt(this.season);
+		dest.writeString(this.showName);
+		dest.writeString(this.showStatus);
+		dest.writeInt(this.tvDbId);
+		dest.writeInt(this.weekday);
+	}
+
+	public static final Creator<Schedule> CREATOR = new Creator<Schedule>() {
+		@Override
+		public Schedule createFromParcel(Parcel in) {
+			return new Schedule(in);
+		}
+
+		@Override
+		public Schedule[] newArray(int size) {
+			return new Schedule[size];
+		}
+	};
 }
