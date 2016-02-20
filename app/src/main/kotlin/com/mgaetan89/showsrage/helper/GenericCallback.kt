efd1a0797b0ae5fedae1a6c9d36ede5a8d5cca18
Gaëtan Muller <m.gaetan89@gmail.com>
2016-02-20 13:50:27 +0100

    #54: More Kotlin

---
+++ app/src/main/kotlin/com/mgaetan89/showsrage/helper/GenericCallback.kt
@@ -0,0 +1,31 @@
+package com.mgaetan89.showsrage.helper
+
+import android.support.v4.app.FragmentActivity
+import android.widget.Toast
+import com.mgaetan89.showsrage.model.GenericResponse
+import retrofit.Callback
+import retrofit.RetrofitError
+import retrofit.client.Response
+import java.lang.ref.WeakReference
+
+open class GenericCallback(activity: FragmentActivity) : Callback<GenericResponse> {
+    val activityReference: WeakReference<FragmentActivity>
+
+    init {
+        this.activityReference = WeakReference(activity)
+    }
+
+    override fun failure(error: RetrofitError?) {
+        error?.printStackTrace()
+    }
+
+    override fun success(genericResponse: GenericResponse?, response: Response?) {
+        val activity = this.getActivity() ?: return
+
+        Toast.makeText(activity, genericResponse?.message, Toast.LENGTH_SHORT).show()
+    }
+
+    protected fun getActivity(): FragmentActivity? {
+        return this.activityReference.get()
+    }
+}
