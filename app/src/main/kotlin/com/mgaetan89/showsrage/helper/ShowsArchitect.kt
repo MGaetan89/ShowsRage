2b89f5ef89dcb592eebe4374af68754b5d6b8cd4
Gaëtan Muller <m.gaetan89@gmail.com>
2016-05-10 22:44:04 +0200

    Temp fix for tests

--- app/src/main/kotlin/com/mgaetan89/showsrage/helper/ShowsArchitect.kt
+++ app/src/main/kotlin/com/mgaetan89/showsrage/helper/ShowsArchitect.kt
@@ -1,18 +1,19 @@
 package com.mgaetan89.showsrage.helper
 
 import android.widget.TextView
-import com.mgaetan89.showsrage.model.Show
 import io.kolumbus.Architect
-import io.kolumbus.activity.TableActivity
 
 class ShowsArchitect : Architect() {
     override fun displayInt(textView: TextView, value: Int) {
         val show = RealmManager.getShow(value)
 
         if (show != null) {
+            // TODO
+            /*
             textView.setOnClickListener {
                 TableActivity.start(textView.context, Show::class.java, arrayOf(show))
             }
+            */
 
             super.displayString(textView, "[$value] ${show.showName}")
         } else {
