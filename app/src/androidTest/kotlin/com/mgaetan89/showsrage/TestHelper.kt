0cbe0530e14032ef2f106d67accc5d961134c156
Gaëtan Muller <m.gaetan89@gmail.com>
2016-09-26 22:29:35 +0200

    Add a couple tests

--- app/src/androidTest/kotlin/com/mgaetan89/showsrage/TestHelper.kt
+++ app/src/androidTest/kotlin/com/mgaetan89/showsrage/TestHelper.kt
@@ -5,15 +5,12 @@ import com.mgaetan89.showsrage.helper.Migration
 import io.realm.Realm
 import io.realm.RealmConfiguration
 
-fun initRealm(context: Context, testContext: Context?) {
+private const val REALM_FILENAME = "test.realm"
+
+fun initRealm(context: Context, testContext: Context) {
     val configuration = RealmConfiguration.Builder(context).let {
-        if (testContext != null) {
-            val testFile = "test.realm"
-
-            it.assetFile(testContext, testFile)
-            it.name(testFile)
-        }
-
+        it.assetFile(testContext, REALM_FILENAME)
+        it.name(REALM_FILENAME)
         it.schemaVersion(Constants.DATABASE_VERSION)
         it.migration(Migration())
         it.build()
