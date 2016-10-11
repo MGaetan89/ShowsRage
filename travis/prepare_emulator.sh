e756b26846ad2280027342239e9395735c94e36b
Gaëtan Muller <m.gaetan89@gmail.com>
2016-10-11 22:03:53 +0200

    One more try

--- travis/prepare_emulator.sh
+++ travis/prepare_emulator.sh
@@ -2,8 +2,6 @@
 
 if [[ $ANDROID_ABI ]]; then
 	echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI
-	sed -i.bak 's/hw.ramSize=512/hw.ramSize=1536/' $HOME/.android/avd/test.avd/config.ini
-	sed -i.bak 's/vm.heapSize=48/vm.heapSize=64/' $HOME/.android/avd/test.avd/config.ini
 	emulator -avd test -no-audio -no-window &
 	android-wait-for-emulator
 	adb shell input keyevent 82 &
