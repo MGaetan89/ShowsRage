750c75acbe3d6ac04a5fba347b99521e79151609
Gaëtan Muller <m.gaetan89@gmail.com>
2016-10-11 22:42:11 +0200

    Update Travis

--- travis/prepare_emulator.sh
+++ travis/prepare_emulator.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
+touch build/logcat.log
+
 if [[ $ANDROID_ABI ]]; then
 	echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI
 	emulator -avd test -no-audio -no-window &
 	android-wait-for-emulator
 	adb shell input keyevent 82 &
+	adb logcat -c
+	adb logcat > build/logcat.log &
 fi
