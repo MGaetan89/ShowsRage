f4a1c88c4a23bdac1b19b6e924d865605f3d9b67
Gaëtan Muller <m.gaetan89@gmail.com>
2016-11-01 19:09:41 +0100

    Change the way Realm is initialised

--- app/src/androidTest/kotlin/com/mgaetan89/showsrage/TestActivity.kt
+++ app/src/androidTest/kotlin/com/mgaetan89/showsrage/TestActivity.kt
@@ -1,6 +1,7 @@
 package com.mgaetan89.showsrage
 
 import android.os.Bundle
+import android.support.test.InstrumentationRegistry
 import android.support.v7.app.AppCompatActivity
 import com.mgaetan89.showsrage.helper.Utils
 
@@ -8,6 +9,6 @@ class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Utils.initRealm(this, "test.realm", deleteRealm = true)
+        Utils.initRealm(InstrumentationRegistry.getContext(), "test.realm", deleteRealm = true)
     }
 }
