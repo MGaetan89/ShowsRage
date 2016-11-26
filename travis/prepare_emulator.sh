a2cd4809231e8f54b313b4c69ddc134e46393bcb
Gaëtan Muller <m.gaetan89@gmail.com>
2016-11-26 11:45:27 +0100

    Update Travis configuration 11

--- travis/prepare_emulator.sh
+++ travis/prepare_emulator.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 if [[ $ANDROID_ABI ]]; then
-	echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI
-	emulator -memory 1536 -avd test -no-audio -no-skin -no-window -wipe-data &
+	EMULATOR_NAME="test_$ANDROID_TARGET_$ANDROID_ABI"
+
+	echo no | android create avd --force -n $EMULATOR_NAME -t $ANDROID_TARGET --abi $ANDROID_ABI
+	emulator -memory 1536 -avd $EMULATOR_NAME -no-audio -no-skin -no-window -wipe-data &
 	android-wait-for-emulator
 	adb shell input keyevent 82 &
 fi
