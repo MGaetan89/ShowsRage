1a8ded9ecc657c3031128d439eed99f5aaf58086
Gaëtan Muller <m.gaetan89@gmail.com>
2016-12-09 22:54:39 +0100

    Fix IllegalStateException

--- app/src/main/kotlin/com/mgaetan89/showsrage/fragment/ShowsSectionFragment.kt
+++ app/src/main/kotlin/com/mgaetan89/showsrage/fragment/ShowsSectionFragment.kt
@@ -284,10 +284,10 @@ class ShowsSectionFragment : Fragment(), RealmChangeListener<RealmResults<Show>>
                     realm.close()
                 }
 
-                filteredShows.filter { it.isValid && it.indexerId == indexerId }.forEach {
-                    it.stat = realmStat ?: it.stat
+                filteredShows.filter { it.isValid && it.indexerId == indexerId }.forEachIndexed { i, show ->
+                    show.stat = realmStat ?: show.stat
 
-                    fragment.adapter?.notifyItemChanged(filteredShows.indexOf(it), Constants.Payloads.SHOWS_STATS)
+                    fragment.adapter?.notifyItemChanged(i, Constants.Payloads.SHOWS_STATS)
                 }
 
                 if (shows.isValid) {
