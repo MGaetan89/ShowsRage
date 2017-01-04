86174a52173d3f59be5850c686e1b678089decc2
Gaëtan Muller <m.gaetan89@gmail.com>
2017-01-04 20:19:48 +0100

    One more try 4

--- app/src/androidTest/kotlin/com/mgaetan89/showsrage/TestActivity.kt
+++ app/src/androidTest/kotlin/com/mgaetan89/showsrage/TestActivity.kt
@@ -1,5 +1,24 @@
 package com.mgaetan89.showsrage
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.mgaetan89.showsrage.helper.Migration
+import io.realm.Realm
+import io.realm.RealmConfiguration
 
-class TestActivity : AppCompatActivity()
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Realm.init(this)
+
+        val configuration = RealmConfiguration.Builder()
+                .assetFile("test.realm")
+                .schemaVersion(Constants.DATABASE_VERSION)
+                .migration(Migration())
+                .build()
+
+        Realm.deleteRealm(configuration)
+        Realm.setDefaultConfiguration(configuration)
+    }
+}
--- app/src/androidTest/kotlin/com/mgaetan89/showsrage/extension/realm/RealmTest.kt
+++ app/src/androidTest/kotlin/com/mgaetan89/showsrage/extension/realm/RealmTest.kt
@@ -1,13 +1,9 @@
 package com.mgaetan89.showsrage.extension.realm
 
 import android.os.Looper
-import android.support.test.InstrumentationRegistry
 import android.support.test.rule.ActivityTestRule
-import com.mgaetan89.showsrage.Constants
 import com.mgaetan89.showsrage.TestActivity
-import com.mgaetan89.showsrage.helper.Migration
 import io.realm.Realm
-import io.realm.RealmConfiguration
 import io.realm.setContext
 import org.junit.After
 import org.junit.AfterClass
@@ -24,17 +20,6 @@ abstract class RealmTest {
 
     @Before
     fun configureRealm() {
-        Realm.init(InstrumentationRegistry.getContext())
-
-        val configuration = RealmConfiguration.Builder()
-                .assetFile("test.realm")
-                .schemaVersion(Constants.DATABASE_VERSION)
-                .migration(Migration())
-                .build()
-
-        Realm.deleteRealm(configuration)
-        Realm.setDefaultConfiguration(configuration)
-
         this.realm.isAutoRefresh = false
     }
 
