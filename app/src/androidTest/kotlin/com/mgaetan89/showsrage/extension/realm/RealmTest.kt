08e8247f73b902ca1dd2afc87b51b7372e9fe12f
Gaëtan Muller <m.gaetan89@gmail.com>
2017-03-19 14:33:39 +0100

    Attempt to fix Realm tests 6

--- app/src/androidTest/kotlin/com/mgaetan89/showsrage/extension/realm/RealmTest.kt
+++ app/src/androidTest/kotlin/com/mgaetan89/showsrage/extension/realm/RealmTest.kt
@@ -2,11 +2,8 @@ package com.mgaetan89.showsrage.extension.realm
 
 import android.os.Looper
 import android.support.test.rule.ActivityTestRule
-import com.mgaetan89.showsrage.Constants
 import com.mgaetan89.showsrage.TestActivity
-import com.mgaetan89.showsrage.helper.Migration
 import io.realm.Realm
-import io.realm.RealmConfiguration
 import org.junit.After
 import org.junit.AfterClass
 import org.junit.Before
@@ -22,15 +19,6 @@ abstract class RealmTest {
 
     @Before
     fun configureRealm() {
-        val configuration = RealmConfiguration.Builder()
-                .assetFile("test.realm")
-                .schemaVersion(Constants.DATABASE_VERSION)
-                .migration(Migration())
-                .build()
-
-        Realm.deleteRealm(configuration)
-        Realm.setDefaultConfiguration(configuration)
-
         this.realm.isAutoRefresh = false
     }
 
