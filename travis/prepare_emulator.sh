805bd709ee5993a88a110d236a2c456bbbbfd734
Gaëtan Muller <m.gaetan89@gmail.com>
2017-10-17 21:36:21 +0200

    Update Travis configuration

--- travis/prepare_emulator.sh
+++ travis/prepare_emulator.sh
@@ -3,7 +3,7 @@
 if [[ $ANDROID_ABI ]]; then
 	EMULATOR_NAME="test_${ANDROID_TARGET}_${ANDROID_ABI}"
 
-	echo no | android create avd --force -n $EMULATOR_NAME -k "system-images;${ANDROID_TARGET};google_apis;${ANDROID_ABI}"
+	echo no | android create avd -f -n $EMULATOR_NAME -t $ANDROID_TARGET -b $ANDROID_ABI -g "google_apis"
 	emulator -memory 1536 -avd $EMULATOR_NAME -no-audio -no-skin -no-window -wipe-data &
 	android-wait-for-emulator
 	adb shell input keyevent 82 &
